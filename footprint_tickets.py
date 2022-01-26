da0d993aac0b02867d2f055cb7a6fd770e8c2f31
Katlin <kvsampso@purdue.edu>
2022-01-26 13:18:24 -0500

    created footprint_tickets.py

---
+++ footprint_tickets.py
@@ -0,0 +1,17 @@
+import Footprints_Python as foot
+
+
+def ticket_details(user, pwd, project_id, ticket_id):
+    foot_connection = foot.Connection(
+        'support.purdue.edu', user, pwd)
+    return foot_connection.get_ticket_details(project_id, ticket_id)
+
+
+def ticket_search(user, pwd, key, project_id):
+    foot_connection = foot.Connection(
+        'support.purdue.edu', user, pwd)
+    return foot_connection.search_tickets(key, project_id)
+
+
+if __name__ == "__main__":
+    pass
