e38c22b07c145ec6d30e5a7856321a0c1bf1fdbb
Katlin <kvsampso@purdue.edu>
2022-03-07 13:50:28 -0500

    adding details to ticket_close

--- footprints_v11/footprint_tickets.py
+++ footprints_v11/footprint_tickets.py
@@ -25,10 +25,30 @@ def ticket_create(user, pwd, project_id, title, details):
 #     return foot_connection.ticket_create(project_id, title, details)
 
 
-def ticket_close(user, pwd, project_id, ticket_number):
+def ticket_close(
+    user,
+    pwd,
+    project_id,
+    ticket_number,
+    resolution='Completed__bSuccessfully',
+    assignees=None,
+    service_offering='Wired__bCampus__bNetwork__bServices',
+    campus='West__bLafayette',
+    tech_note='Closed with footprints automation'):
+    '''
+    '''
     foot_connection = foot.Connection(
         'support.purdue.edu', user, pwd)
-    return foot_connection.ticket_close(project_id, ticket_number)
+    ticket_return = foot_connection.ticket_close(
+        project_id, 
+        ticket_number, 
+        resolution=resolution,
+        assignees=assignees,
+        service_offering=service_offering,
+        campus=campus,
+        tech_note=tech_note)
+
+    return ticket_return
 
 
 if __name__ == "__main__":
